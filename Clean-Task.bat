a0c7badddd9b653696ec90ae257e656e8be5ad37
Khanggggggggggg <78673415+Khanggggggggggg@users.noreply.github.com>
2022-06-15 09:22:07 +0700

    Add files via upload
    aasa

---
+++ Clean-Task.bat
@@ -0,0 +1,13 @@
+@echo off
+taskkill /F /IM msedge.exe
+Taskkill /f /im ManyCamService.exe
+Taskkill /f /im ManyCam.exe
+Taskkill /f /im Grammarly.Desktop.exe
+Taskkill /f /im Zalo.exe
+Taskkill /f /im ZaloCap.exe
+Taskkill /f /im ZaviMeet.exe
+Taskkill /f /im zWebview2Agent.exe
+Taskkill /f /im Zoom.exe
+Taskkill /f /im AnyDesk.exe
+echo Cleared all tasks, enjoy your game ;)
+PAUSE
